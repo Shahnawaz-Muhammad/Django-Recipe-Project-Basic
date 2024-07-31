a8351b2bde28bf9b3c0804d25c076174a18fb1a0
Shahnawaz-Muhammad <shahnawazbaltee55@gmail.com>
2024-07-31 17:15:43 +0500

    user registration

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
@@ -47,3 +48,36 @@ def about(request):
 def contact(request):
     context={'page': 'Contact'}
     return render(request, 'main/contact.html', context)
+  
+def login(request):
+    context={'page': 'Login'}
+    return render(request, 'auth/login.html', context)
+  
+    
+def register(request):
+  if request.method == "POST":
+    first_name = request.POST.get('first_name')
+    last_name = request.POST.get('last_name')
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    
+    user = User.objects.filter(username = username)
+
+    if user.exists():
+      messages.warning(request, "User already exits!. ")
+      return redirect('/register/')
+    
+    user = User.objects.create(
+      first_name = first_name,
+      last_name = last_name,
+      username = username,
+      )
+    
+    user.set_password(password)
+    user.save()
+    
+    messages.success(request, "User created Successfull!")
+    
+    return redirect('/register/')
+
+  return render(request, 'auth/register.html')
